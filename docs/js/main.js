0b81b359117cefcdc9ae82360abb9ce904e303b4
Matthew Morgan <Matthew.Morgan@ibm.com>
2020-07-18 11:43:46 -0400

    1
    1

--- docs/js/main.js
+++ docs/js/main.js
@@ -470,3 +470,19 @@ for (i = 0; i < coll.length; i++) {
   });
 } 
 
+
+
+//------- MATTS --------// 
+function openJob(evt, jobName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(jobName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
